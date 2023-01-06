e31158e1b351d61ffc00723ffbce0442f434618c
Michael <michaeljohannpeter@gmail.com>
2023-01-06 21:25:29 +0100

    refactor: implement finding 'kinds:' as function

--- lua/kustomize/kinds.lua
+++ lua/kustomize/kinds.lua
@@ -1,6 +1,15 @@
 local utils = require("kustomize.utils")
 local M = {}
 
+M.find_kinds = function(content)
+  local kinds = {}
+  for _, line in pairs(content) do
+    local kind = string.match(line, "^kind:%s*(%a+).*")
+    table.insert(kinds, kind)
+  end
+  return kinds
+end
+
 M.list = function()
   local bufnr = vim.api.nvim_win_get_buf(0)
   local ft = vim.api.nvim_buf_get_option(bufnr, "ft")
@@ -8,11 +17,7 @@ M.list = function()
     error("Expecting yaml file")
   end
   local content = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  local kinds = {}
-  for _, line in pairs(content) do
-    local kind = string.match(line, "^kind:%s*(%a+).*")
-    table.insert(kinds, kind)
-  end
+  local kinds = M.find_kinds(content)
   local win, buf = utils.create_output()
   vim.api.nvim_win_set_buf(win, buf)
   vim.api.nvim_buf_set_name(buf, "Kinds #" .. buf)
