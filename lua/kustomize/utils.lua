cb8726fc0effdcf28711c9024a0f853e2323d997
Michael <michaeljohannpeter@gmail.com>
2023-10-21 13:07:30 +0200

    fix: path separator

--- lua/kustomize/utils.lua
+++ lua/kustomize/utils.lua
@@ -39,7 +39,7 @@ end
 ---returns OS dependant path separator
 ---@return string
 M.path_separator = function()
-  local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("unix") == 1
+  local is_windows = vim.fn.has("win32") == 1
   if is_windows == true then
     return "\\"
   else
