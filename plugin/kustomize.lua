483d2651d77f80cbf7c4602e4ed3c42360c9a5e8
Michael <michaeljohannpeter@gmail.com>
2024-10-12 00:15:26 +0200

    chore: Fix tests

--- plugin/kustomize.lua
+++ plugin/kustomize.lua
@@ -20,7 +20,7 @@ vim.api.nvim_create_user_command("KustomizeRun", function(opts)
   local choice = opts.fargs[1]
   local cmd = config.options.run[choice].cmd
   local args = config.options.run[choice].args
-  local timeout = config.options.run[choice].timeout or 5000
+  local timeout = config.options.run[choice].timeout or 5000 -- TODO: refactor in one datastructure
   run.run_checked(cmd, args, timeout)
 end, {
   desc = "Run commands",
