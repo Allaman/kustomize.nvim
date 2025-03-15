e5b1d11003daa36b74b0b55a5495f294b48813aa
Michael <github@mjpeter.de>
2025-03-15 21:23:28 +0100

    chore(build): Fix tests

--- tests/kustomize/test_build.lua
+++ tests/kustomize/test_build.lua
@@ -22,13 +22,13 @@ local want2 = "no such file or directory"
 
 describe("kustomize build", function()
   it("valid kustomization.yaml", function()
-    local err2, get2 = build._kustomize_build("tests/kustomize/test_data/build/pass")
+    local err2, get2 = build._kustomize_build("tests/kustomize/test_data/build/pass", {})
     eq(err2, {})
     eq(get2, want1)
   end)
 
   it("missing resources", function()
-    local err2, get2 = build._kustomize_build("tests/kustomize/test_data/build/fail")
+    local err2, get2 = build._kustomize_build("tests/kustomize/test_data/build/fail", {})
     local err_string = table.concat(err2, "\n")
     local match = string.find(err_string, want2)
     neq(match, {})
@@ -36,8 +36,8 @@ describe("kustomize build", function()
   end)
 
   it("wrong additional argument", function()
-    config.options.build.additional_args = { "-foo" }
-    local err3, _ = build._kustomize_build("tests/kustomize/test_data/build/fail")
+    local additional_args = { "-foo" }
+    local err3, _ = build._kustomize_build("tests/kustomize/test_data/build/fail", additional_args)
     eq(err3, { "Error: unknown shorthand flag: 'f' in -foo" })
   end)
 end)
